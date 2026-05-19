3949d04b068aa30f251685a98bfd98733f7b5a1e
CappyTheeCom <rami.alasady0407@gmail.com>
2026-05-19 21:14:09 +1000

    Update TripEmbeddableId.java

--- backend/src/main/java/com/server/backend/tripinfo/TripEmbeddableId.java
+++ backend/src/main/java/com/server/backend/tripinfo/TripEmbeddableId.java
@@ -5,12 +5,14 @@ import java.io.Serializable;
 import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Embeddable
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode
 public class TripEmbeddableId implements Serializable {
     private String planeId;
     private String departure;
